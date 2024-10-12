bbe9ccc22e06253c16c2a93e624a4ce2b624893a
awprevite <awprevite@gmail.com>
2024-10-11 22:17:02 -0400

    Final version for submission

--- PlayGame.py
+++ PlayGame.py
@@ -5,9 +5,18 @@ from HumanPlayer import *
 
 print("\n***********************************\n\nStarting the poker game! Good luck!\n\n***********************************\n")
 
+# Set up game parameters
 config = setup_config(max_round=100, initial_stack=500, small_blind_amount=10)
-config.register_player(name="Andrew", algorithm=HumanPlayer("Andrew"))
-config.register_player(name="Daniel", algorithm=SmartPlayer("Daniel"))
-config.register_player(name="Anthony", algorithm=EmotionalPlayer("Anthony"))
-config.register_player(name="Evan", algorithm=SmartPlayer("Evan"))
+
+# Human players
+config.register_player(name="Human", algorithm=HumanPlayer("Human"))
+
+# Smart players
+config.register_player(name="Smart 1", algorithm=SmartPlayer("Smart 1"))
+config.register_player(name="Smart 2", algorithm=SmartPlayer("Smart 2"))
+
+# Emotional players
+config.register_player(name="Emotional 1", algorithm=EmotionalPlayer("Emotional 1"))
+
+# Begin game
 game_result = start_poker(config, verbose=1)
